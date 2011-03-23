0aee4b62e1dfe90b7f25026daafd93542f636597
amadeus <amadeusdemarzi@gmail.com>
2011-03-23 10:03:50 -0700

    A bunch of tweaks and fixes: * Renamed bind/unbind/trigger to subscribe/unsubscribe/publish due to .bind meaning something entirely different in ES5 * Added JSLint validation * Added return this; to all methods to allow for chaining * Updated tests and examples to work with new API

--- microevent.js
+++ microevent.js
@@ -1,6 +1,6 @@
 /**
  * MicroEvent - to make any js object an event emitter (server or browser)
- * 
+ *
  * - pure javascript - server compatible, browser compatible
  * - dont rely on the browser doms
  * - super simple - you get it immediatly, no mistery, no magic involved
@@ -9,24 +9,27 @@
  *   - make it safer to use
 */
 
-var MicroEvent	= function(){}
+var MicroEvent	= function(){};
 MicroEvent.prototype	= {
-	bind	: function(event, fct){
+	subscribe	: function(event, fct){
 		this._events = this._events || {};
 		this._events[event] = this._events[event]	|| [];
 		this._events[event].push(fct);
+		return this;
 	},
-	unbind	: function(event, fct){
+	unsubscribe	: function(event, fct){
 		this._events = this._events || {};
-		if( event in this._events === false  )	return;
+		if( event in this._events === false  )	return this;
 		this._events[event].splice(this._events[event].indexOf(fct), 1);
+		return this;
 	},
-	trigger	: function(event /* , args... */){
+	publish	: function(event /* , args... */){
 		this._events = this._events || {};
-		if( event in this._events === false  )	return;
+		if( event in this._events === false  )	return this;
 		for(var i = 0; i < this._events[event].length; i++){
-			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1))
+			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
 		}
+		return this;
 	}
 };
 
@@ -38,13 +41,13 @@ MicroEvent.prototype	= {
  * @param {Object} the object which will support MicroEvent
 */
 MicroEvent.mixin	= function(destObject){
-	var props	= ['bind', 'unbind', 'trigger'];
+	var props	= ['subscribe', 'unsubscribe', 'publish'];
 	for(var i = 0; i < props.length; i ++){
 		destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
 	}
-}
+};
 
 // export in common js
 if( typeof module !== "undefined" && ('exports' in module)){
-	module.exports	= MicroEvent
+	module.exports	= MicroEvent;
 }
