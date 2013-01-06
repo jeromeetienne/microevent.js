b569db7a05a3d6f1d9b285fceeae82403f4d590f
Andrew Appleton <andrew@floatleft.com>
2013-01-06 11:43:38 +0000

    Use aliases in mixin function

--- microevent.js
+++ microevent.js
@@ -43,7 +43,7 @@ MicroEvent.prototype.emit = MicroEvent.prototype.trigger;
  * @param {Object} the object which will support MicroEvent
 */
 MicroEvent.mixin  = function(destObject){
-  var props = ['bind', 'unbind', 'trigger'];
+  var props = ['bind', 'unbind', 'trigger', 'on', 'off', 'emit'];
   for(var i = 0; i < props.length; i ++){
     destObject.prototype[props[i]]  = MicroEvent.prototype[props[i]];
   }
