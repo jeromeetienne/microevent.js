a8293fe9571ea4e609d51ec906d627e64dfb8eba
Ali Najafizadeh <a.najafizadeh@gmail.com>
2014-07-11 16:30:18 -0400

    added once and some performance improvements

--- microevent.js
+++ microevent.js
@@ -12,21 +12,44 @@
 var MicroEvent	= function(){};
 MicroEvent.prototype	= {
 	bind	: function(event, fct){
-		this._events = this._events || {};
-		this._events[event] = this._events[event]	|| [];
-		this._events[event].push(fct);
+        this._events = this._events || {};
+        this._events[event] = this._events[event] || [];
+        this._events[event].push(fct);
 	},
+    once    : function(event, fct) {
+        this._once = this._once || [];
+        this._once[event].push(fct);
+    },
 	unbind	: function(event, fct){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		this._events[event].splice(this._events[event].indexOf(fct), 1);
+        this._events = this._events || {};
+        if( event in this._events === false )	return;
+        if ( fct ) this._events[event].splice(this._events[event].indexOf(fct), 1);
+        else this._events[event] = [];
 	},
 	trigger	: function(event /* , args... */){
-		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		for(var i = 0; i < this._events[event].length; i++){
-			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
-		}
+        var targets,
+            args,
+            i,
+            length;
+
+        this._events = this._events || {};
+
+        targets = this._events[event];
+        if(targets) {
+            length = targets.length;
+            if (length) args = Array.prototype.slice.call(arguments, 1);
+            for (i = 0; i < length; i++) targets[i].apply(this, args);
+        }
+
+        targets = this._once[event];
+        if (targets) {
+            length = targets.length;
+            if (!args && length) {
+                args = Array.prototype.slice.call(arguments, 1);
+                for (i = 0; i < length; i++) targets[i].apply(this, args);
+                delete this._once[event];
+            }
+        }
 	}
 };
 
@@ -38,7 +61,7 @@ MicroEvent.prototype	= {
  * @param {Object} the object which will support MicroEvent
 */
 MicroEvent.mixin	= function(destObject){
-	var props	= ['bind', 'unbind', 'trigger'];
+	var props	= ['bind', 'once', 'unbind', 'trigger'];
 	for(var i = 0; i < props.length; i ++){
 		if( typeof destObject === 'function' ){
 			destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
