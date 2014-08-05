ef666879e2a48d49503c5092021086f7c6841815
Jeroen Ransijn <jeroen@aanzee.nl>
2014-08-05 11:25:20 +0200

    Overall update
    - obj.bind is now obj.on
    - obj.unbind is now obj.off
    - Added chainability `obj.on('a', fn).on('b', fn)`
    - Added on all events function `on('*', function (eventName) { })` and
    `trigger('*')`
    - Some code reformatted

--- microevent.js
+++ microevent.js
@@ -1,32 +1,42 @@
 /**
  * MicroEvent - to make any js object an event emitter (server or browser)
- * 
+ * ------------------------------------------------------
+ * Edited by Jeroen Ransijn in august 2014
+ * - obj.bind is now obj.on
+ * - obj.unbind is now obj.off
+ * - Added chainability `obj.on('a', fn).on('b', fn)`
+ * - Added on all events function `on('*', function (eventName) { })` and `trigger('*')`
+ * - Some code reformatted
+ * ------------------------------------------------------
  * - pure javascript - server compatible, browser compatible
  * - dont rely on the browser doms
  * - super simple - you get it immediatly, no mistery, no magic involved
- *
- * - create a MicroEventDebug with goodies to debug
- *   - make it safer to use
 */
-
-var MicroEvent	= function(){};
-MicroEvent.prototype	= {
-	bind	: function(event, fct){
+var MicroEvent = function () {};
+MicroEvent.prototype = {
+	on: function (event, fct) {
 		this._events = this._events || {};
-		this._events[event] = this._events[event]	|| [];
+		this._events[event] = this._events[event] || [];
 		this._events[event].push(fct);
+		return this;
 	},
-	unbind	: function(event, fct){
+	off: function (event, fct) {
 		this._events = this._events || {};
-		if( event in this._events === false  )	return;
+		if ( ! (event in this._events) ) return this;
 		this._events[event].splice(this._events[event].indexOf(fct), 1);
+		return this;
 	},
-	trigger	: function(event /* , args... */){
+	trigger: function (event) {
 		this._events = this._events || {};
-		if( event in this._events === false  )	return;
-		for(var i = 0; i < this._events[event].length; i++){
-			this._events[event][i].apply(this, Array.prototype.slice.call(arguments, 1));
+		var argsAll = Array.prototype.slice.call(arguments, 0), args = argsAll.slice(1);
+		argsAll.unshift('*');
+		if (event !== '*') this.trigger.apply(this, argsAll);
+		if ( ! (event in this._events) ) return this;
+		var eventsLength = this._events[event].length;
+		for (var i = 0; i < eventsLength; i++) {
+			this._events[event][i].apply(this, args);
 		}
+		return this;
 	}
 };
 
@@ -37,16 +47,16 @@ MicroEvent.prototype	= {
  *
  * @param {Object} the object which will support MicroEvent
 */
-MicroEvent.mixin	= function(destObject){
-	var props	= ['bind', 'unbind', 'trigger'];
-	for(var i = 0; i < props.length; i ++){
-		if( typeof destObject === 'function' ){
+MicroEvent.mixin = function (destObject) {
+	var props	= ['on', 'off', 'trigger'];
+	for (var i = 0; i < 3 /* props.length */; i++) {
+		if ( typeof destObject === 'function' ) {
 			destObject.prototype[props[i]]	= MicroEvent.prototype[props[i]];
-		}else{
+		} else {
 			destObject[props[i]] = MicroEvent.prototype[props[i]];
 		}
 	}
-}
+};
 
 // export in common js
 if( typeof module !== "undefined" && ('exports' in module)){
